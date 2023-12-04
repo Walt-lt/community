8fe30dfbbe40f4b0b750c00914aa24ac60488390
Walt-lt <152582307+Walt-lt@users.noreply.github.com>
2023-12-04 08:12:30 +0800

    extract github from code toproperties

--- src/main/java/com/ltstudy/community/Mapper/UserMapper.java
+++ src/main/java/com/ltstudy/community/Mapper/UserMapper.java
@@ -3,10 +3,9 @@ package com.ltstudy.community.Mapper;
 import com.ltstudy.community.Model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert  into  appuser (account_id, name, token, gmt_create, gmt_modified) values(#{account_id},#{name},#{token},#{gmt_create},#{gmt_modified})")
+   @Insert("insert into liu.user (account_id,name,token,gmt_create,gmt_modified) VALUES ( #{account_id},#{name},#{token},#{gmt_create},#{gmt_modified} )")
     void insert(User user);
 }
